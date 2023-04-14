96b1ca90d65556721bf26a04547673e02ca9a3f2
Vishwas Rana <67436082+Vishwas2@users.noreply.github.com>
2023-04-14 18:04:37 +0530

    Add files via upload

---
+++ Day_001_Hello_World/Day001.java
@@ -0,0 +1,25 @@
+public class Day001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World !!!");
+        // this is a single line comment in Java.
+        // comments are ignored by the compiler and the interpreter.
+        // comments can be used by the progammer to make the code more readable.
+
+        /*
+         * Basic structure of a Java program :
+         * functions -> a group of code that can be used to perform a specific task like
+         * addition. They provide reusability and modularity to a Java program.
+         * classes -> class defines the structure and behavior of objects that belong to
+         * the same type. Classes consists of fields (variables) and methods
+         * (functions).
+         */
+
+        /*
+         * Naming conventions in Java :
+         * For classes we use PascalConvention -> first letter of all words is capital.
+         * (ex : FirstVariable)
+         * For functions we use camelCaseConvention-> first letter of the first word is
+         * small. Rest all are big. (ex : firstVariable)
+         */
+    }
+}
