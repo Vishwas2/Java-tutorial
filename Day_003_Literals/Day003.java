d007669905ebce52c022f16dee605db7d6714cad
Vishwas Rana <67436082+Vishwas2@users.noreply.github.com>
2023-04-15 21:04:06 +0530

    Add files via upload

---
+++ Day_003_Literals/Day003.java
@@ -0,0 +1,20 @@
+public class Day003 {
+    public static void main(String[] args) {
+        // Literal : Any constant value which can be assinged to a variable is called
+        // literal/constant.
+
+        /*
+         * 101 -> integer literal
+         * 10.1 -> double literal (defult type for decimals)
+         * 10.1f -> float literal
+         * 'A' -> character literal
+         * true -> boolean literal
+         * "Harry" -> String literal
+         * 56666666L -> long literal
+         */
+
+        String str = "Vishwas";
+        // NOTE THAT THE S OF STRING SHOULD ALWAYS BE CAPITAL
+        System.out.println(str);
+    }
+}
