1128315dd78dbbc9b6f17bd6b914ad89a7c4a394
Vishwas Rana <67436082+Vishwas2@users.noreply.github.com>
2023-04-17 21:40:56 +0530

    Add files via upload

---
+++ Day_006_Operators_And_Its_Types/Day006.java
@@ -0,0 +1,105 @@
+public class Day006 {
+    public static void main(String[] args) {
+        // Operators are used to perform operations on variables and values
+        // 10 + 5 = 15
+        // here + is the operator and 10 and 5 are operands
+
+        /*
+         * Types of operators :
+         * Arithmetic operator -> +,-,*,/,% (modulo operator),++,--
+         * Assignment operator -> =,+=,-=,*=,/=
+         * Comparison operator -> ==,>=,<=
+         * Logical operator -> &&,||,!
+         * bitwise operator -> &,|,^ (operates bitwise)
+         */
+
+        /*
+         * Arithmetic Operators:
+         * Arithmetic operators are used to perform mathematical operations such as
+         * addition, subtraction, multiplication, division, and modulus. Here's a list
+         * of arithmetic operators in Java:
+         * 
+         * Addition (+): Adds two operands together. For example, 2 + 3 equals 5.
+         * Subtraction (-): Subtracts the second operand from the first operand. For
+         * example, 5 - 2 equals 3.
+         * Multiplication (*): Multiplies two operands together. For example, 2 * 3
+         * equals 6.
+         * Division (/): Divides the first operand by the second operand. For example, 6
+         * / 3 equals 2.
+         * Modulus (%): Returns the remainder of the division of the first operand by
+         * the second operand. For example, 7 % 3 equals 1.
+         */
+
+        /*
+         * Assignment Operators:
+         * Assignment operators are used to assign values to variables. Here's a list of
+         * assignment operators in Java:
+         * 
+         * Assignment (=): Assigns the value on the right-hand side to the variable on
+         * the left-hand side. For example, int x = 5; assigns the value 5 to the
+         * variable x.
+         * Addition Assignment (+=): Adds the value on the right-hand side to the
+         * variable on the left-hand side and then assigns the result to the variable on
+         * the left-hand side. For example, x += 3; is equivalent to x = x + 3;.
+         * Subtraction Assignment (-=): Subtracts the value on the right-hand side from
+         * the variable on the left-hand side and then assigns the result to the
+         * variable on the left-hand side. For example, x -= 3; is equivalent to x = x -
+         * 3;.
+         * Multiplication Assignment (*=): Multiplies the value on the right-hand side
+         * with the variable on the left-hand side and then assigns the result to the
+         * variable on the left-hand side. For example, x *= 3; is equivalent to x = x *
+         * 3;.
+         * Division Assignment (/=): Divides the variable on the left-hand side by the
+         * value on the right-hand side and then assigns the result to the variable on
+         * the left-hand side. For example, x /= 3; is equivalent to x = x / 3;.
+         */
+
+        /*
+         * Comparison Operators:
+         * Comparison operators are used to compare two values. They return a boolean
+         * value (true or false) depending on whether the comparison is true or false.
+         * Here's a list of comparison operators in Java:
+         * 
+         * Equal to (==): Returns true if the operands are equal. For example, 2 == 2
+         * returns true.
+         * Not equal to (!=): Returns true if the operands are not equal. For example, 2
+         * != 3 returns true.
+         * Greater than (>): Returns true if the first operand is greater than the
+         * second operand. For example, 3 > 2 returns true.
+         * Less than (<): Returns true if the first operand is less than the second
+         * operand. For example, 2 < 3 returns true.
+         * Greater than or equal to (>=): Returns true if the first operand is greater
+         * than or equal to the second operand. For example, 3 >= 3 returns true.
+         * Less than or equal to (<=): Returns true if the first operand is less than or
+         * equal to the second operand. For example, 2 <= 3 returns true.
+         */
+
+        /*
+         * Logical Operators:
+         * Logical operators are used to perform logical operations such as AND, OR, and
+         * NOT. They return a boolean value (true or false) depending on the result of
+         * the logical operation. Here's a list of logical operators in Java:
+         * 
+         * AND (&&): Returns true if both operands are true. For example, (x > 1) && (x
+         * < 5) returns true if x is between 1 and 5.
+         * OR (||): Returns true if at least one of the operands is true. For example,
+         * (x == 1) || (x == 5) returns true if x is either 1 or 5.
+         * NOT (!): Returns the opposite boolean value of the operand. For example, !(x
+         * == 1) returns true if x is not equal to 1.
+         */
+
+        /*
+         * Bitwise Operators:
+         * Bitwise operators are used to perform operations on individual bits of binary
+         * numbers. Here's a list of bitwise operators in Java:
+         * 
+         * Bitwise AND (&): Performs a bitwise AND operation on the two operands. For
+         * example, 5 & 3 returns 1, which is the binary AND of 101 and 011.
+         * Bitwise OR (|): Performs a bitwise OR operation on the two operands. For
+         * example, 5 | 3 returns 7, which is the binary OR of 101 and 011.
+         * Bitwise XOR (^): Performs a bitwise XOR (exclusive OR) operation on the two
+         * operands. For example, 5 ^ 3 returns 6, which is the binary XOR of 101 and
+         * 011.
+         */
+    }
+}
