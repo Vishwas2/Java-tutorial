bdb004aed0f0463c172c592a23adba3036935ca3
Vishwas Rana <67436082+Vishwas2@users.noreply.github.com>
2023-06-25 18:59:55 +0530

    Add files via upload

---
+++ Day_020_Arrays/Day020.java
@@ -0,0 +1,22 @@
+package Day_020_Arrays;
+
+public class Day020 {
+    public static void main(String[] args) {
+        // Arrays are very useful in Java
+        /*
+         * Suppose we want to store the marks of 500 students
+         * then instead of making 500 variables we can simply create
+         * an array of size 500.
+         */
+        // Arrays start from 0 and goes till n-1 where n is the size of array
+        /*
+         * There are 3 different ways to create an array :
+         * 1. int[] marks;
+         * marks = new int [5];
+         * 
+         * 2. int[] marks = new int[5];
+         * 
+         * 3. int[] marks = {100, 70, 90, 56, 67};
+         */
+    }
+}
