ef2ba25e55475b6074e38cecdff8f05cad9a1e1f
Vishwas Rana <67436082+Vishwas2@users.noreply.github.com>
2023-07-07 22:00:00 +0530

    Add files via upload

---
+++ Day_029_Object_Oriented_Programming/Day029.java
@@ -0,0 +1,16 @@
+// Object Oriented Programming tries to map code instructions with
+// real world making the code short and easier to understand.
+
+// WHAT IS OBJECT ORIENTED PROGRAMMING ?
+/*
+ * Solving a problem by creating objects is one of the most popular
+ * approaches in programming .This is called Object Oriented Programming.
+ */
+
+// DRY -> Don't Repeat Yourself (focuses on code reusability)
+
+// HOW TO MODEL A PROBLEM IN OOPs
+// We identify the following :
+// Noun -> Class -> Employee
+// Adjective -> Attributes -> name, age, salary
+// Verb -> Methods -> getSalary(), increment()
