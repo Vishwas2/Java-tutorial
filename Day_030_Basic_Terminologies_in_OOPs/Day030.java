23244741d9fcc705ad3d4e5d0cdb16e0b7a5c5b5
Vishwas Rana <67436082+Vishwas2@users.noreply.github.com>
2023-07-07 22:00:56 +0530

    Add files via upload

---
+++ Day_030_Basic_Terminologies_in_OOPs/Day030.java
@@ -0,0 +1,19 @@
+// OOPs Terminology
+
+// 1. ABSTRACTION -> Hiding internal details (show only essential info)
+// For example : using a phone without bothering how it was made
+
+// 2. ENCAPSULATION -> The act of putting various components together (in a
+// capsule)
+// For example : laptop is a single entity with wifi + speaker + display +
+// storage in a single box
+/*
+ * In Java encapsulation basically means that sensitive data can be hidden from
+ * the users.
+ */
+
+// 3. INHERITANCE -> The act of deriving new things from existing things.
+// For example : Rickshaw -> E-Rickshaw, Phone -> Smart Phone
+
+// 4. POLYMORPHISM -> One entity many forms
+// Smartphone -> Phone , Smartphone -> Calculator
