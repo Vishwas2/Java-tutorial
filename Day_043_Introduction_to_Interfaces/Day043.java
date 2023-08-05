3654270310134a1831c7bff44d457adc438c973e
Vishwas Rana <67436082+Vishwas2@users.noreply.github.com>
2023-08-05 14:34:15 +0530

    Add files via upload

---
+++ Day_043_Introduction_to_Interfaces/Day043.java
@@ -0,0 +1,21 @@
+package Day_043_Introduction_to_Interfaces;
+
+// Interface in English is a point where two systems meet and interact. For example, you can interact with you TV with the help of buttons.
+// In Java interface is a group of related methods with empty bodies.
+// AN EXAMPLE :
+/*
+interface Bicycle{
+    void applyBrakes(int decrement);
+    void speedUp(int increment);
+    }
+class FireFox implements Bicycle{
+    int speed = 7;
+    void applyBrakes(int decrement){
+        speed = speed-decrement;
+        }
+    void speedUp(int increment){
+        speed = speed + increment;
+        }}
+ */
+
+// The difference between abstract and interfaces will be discussed in great detail in the upcoming days and when you should use which.
