4d5698613929dd299c52706dd4871de4cd201941
Vishwas Rana <67436082+Vishwas2@users.noreply.github.com>
2023-08-06 10:57:20 +0530

    Add files via upload

---
+++ Day_045_Is_Multiple_Inheritance_Allowed_In_Java/Day045.java
@@ -0,0 +1,8 @@
+package Day_045_Is_Multiple_Inheritance_Allowed_In_Java;
+
+// Multiple inheritance is when one class has more than one parent class.
+// Java doesn't support multiple inheritance in classes, only in interfaces. This means a class can implement more than
+// one interface but can only extend one parent class.
+// Python , C++, and many other languages have multiple inheritance. The creators of Java deliberately made this choice
+// because of the diamond problem ( to get clear understanding watch this video :
+// https://www.youtube.com/watch?v=1-JBFJ8Xar0&ab_channel=CodingwithJohn )
