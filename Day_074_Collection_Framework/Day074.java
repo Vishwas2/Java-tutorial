c863eff689c1a1a3e2ef75d7c96408eba6eacbc3
Vishwas Rana <67436082+Vishwas2@users.noreply.github.com>
2023-08-17 02:04:21 +0530

    Add files via upload

---
+++ Day_074_Collection_Framework/Day074.java
@@ -0,0 +1,23 @@
+/*
+COLLECTION FRAMEWORK :
+The Java Collections Framework is a set of classes and interfaces that provide the foundational infrastructure to work with
+and manipulate groups of objects, commonly referred to as collections, in a structured and efficient way.
+ */
+
+/*
+WHY DO WE NEED COLLECTIONS ?
+We need collection for efficient storage and better manipulation of data in Java. For example : We use arrays to store
+integers but what if we want to :
+1. Resize this array ?
+2. Insert an element in between ?
+3. Delete an element in array ?
+4. Apply certain operations to change this array ?
+ */
+/*
+HOW ARE COLLECTIONS AVAILABLE ?
+Collection in Java are available as Classes and Interfaces . Following are few commonly used collections in Java :
+* Arraylist -> For variable size collection
+* Set -> For distinct collection
+* Stack -> A LIFO stack structure
+* HashMap -> For storing key-value pairs
+ */
