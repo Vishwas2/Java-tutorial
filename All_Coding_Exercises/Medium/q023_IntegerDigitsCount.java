856093f76784f52816a9315144ce6ed681fd597f
Vishwas2 <vishwasrana363@gmail.com>
2023-10-03 21:45:28 +0530

    commit

--- All_Coding_Exercises/Medium/q023_IntegerDigitsCount.java
+++ All_Coding_Exercises/Medium/q023_IntegerDigitsCount.java
@@ -1,5 +1,6 @@
 // Create a function that recursively counts the integer's number of digits.
 // For example : count(318) ➞ 3
+// The function should not count zeroes that before the first non-zero number (for example : 012 should give the number of digits as 2)
 
 package Exercises.All_Coding_Exercises.Medium;
 
@@ -7,15 +8,20 @@ import java.util.Scanner;
 
 public class q023_IntegerDigitsCount {
     // Creating a recursive function to count the number of digits :
-//    static int recursiveDigitCount(int integer) {
-//
-//    }
+    static int recursiveDigitCount(int integer) {
+        int sum = 0;
+        if (integer / 10 <= 0) {
+            return 1;
+        }
+        return sum + 1 + recursiveDigitCount(integer / 10);
+    }
 
     public static void main(String[] args) {
         System.out.println("This is a program to count the number of digits entered by the user.");
         System.out.println("Enter the integer : ");
         Scanner scanner = new Scanner(System.in);
         int input = scanner.nextInt();
-
+        System.out.println(recursiveDigitCount(input));
+        scanner.close();
     }
 }
